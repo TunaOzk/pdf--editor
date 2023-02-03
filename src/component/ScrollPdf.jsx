6262c53f167ee3929d35175b75c7f10e78601f28
tozkoc <tunaozkoc325@gmail.com>
2023-02-03 22:31:21 +0300

    style:ScrollPdf style is updated

--- src/component/ScrollPdf.jsx
+++ src/component/ScrollPdf.jsx
@@ -2,39 +2,42 @@ import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
+import { ReactComponent as AddIcon } from '../assets/add.svg';
 
-function ScrollPdf({ docFile }) {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [file, setFile] = useState('./sample.pdf');
-
-  const onDocumentLoadSuccess = (pdf) => {
-    setNumPages(pdf.numPages);
-  };
-
+function ScrollPdf({ docFile, onLoadSuccess, numPages }) {
   return (
-    <div className="flex flex-col">
-      <Document file={docFile} onLoadSuccess={onDocumentLoadSuccess}>
+    <div className="flex flex-col items-center">
+      <AddIcon className=" border-2 rounded-full border-indigo-500" />
+      <Document file={docFile} onLoadSuccess={onLoadSuccess}>
         { [
           ...Array(numPages),
         ].map((value, index) => (
-          <Page
-            className="rounded-md border-4 border-indigo-500 mb-8"
-            key={uuid()}
-            renderTextLayer={false}
-            renderAnnotationLayer={false}
-            width={100}
-            scale={2}
-            pageNumber={index + 1}
-          />
+          <div className="flex flex-col items-center" key={uuid()}>
+            <Page
+              className="rounded-md border-4 border-indigo-500"
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              width={100}
+              scale={2}
+              pageNumber={index + 1}
+            />
+            <div>
+              <AddIcon className=" border-2 rounded-full border-indigo-500" />
+            </div>
+
+          </div>
+
         ))}
       </Document>
+
     </div>
   );
 }
 
 ScrollPdf.propTypes = {
   docFile: PropTypes.string.isRequired,
+  onLoadSuccess: PropTypes.func.isRequired,
+  numPages: PropTypes.number.isRequired,
 };
 
 export default ScrollPdf;
