d0fed328faf1a01462b623e305f2db888dabf6c8
tozkoc <tunaozkoc325@gmail.com>
2023-02-03 22:32:11 +0300

    feat:ScrollPdf props are updated

--- src/component/DragDrop.jsx
+++ src/component/DragDrop.jsx
@@ -14,14 +14,23 @@ function DragDrop() {
     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
       const file = event.dataTransfer.files[0];
       console.log(file);
+      // const reader = new FileReader();
+      // console.log(reader.readAsArrayBuffer(file));
     }
   };
+
+  const handleChange = (event) => {
+    if (event.target.files[0]) {
+      const file = event.target.files[0];
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
 
       <div className="flex items-center justify-center w-1/2 border-4 border-sky-500 rounded-md h-96 group" role="button" tabIndex={0} onKeyDown={() => { }} onClick={handleClick} onDragOver={handleDrop} onDragEnter={handleDrop} onDrop={handleDrop}>
         <h1 className="opacity-50 text-xl transition ease-in-out group-hover:-translate-y-2">Drag or click to upload your PDF file</h1>
-        <input className="hidden" type="file" id="file" ref={inputFile} />
+        <input className="hidden" type="file" id="file" ref={inputFile} onChange={handleChange} />
       </div>
 
       <form className="flex mt-2 justify-between w-1/2">
--- src/pages/PdfEditPage.jsx
+++ src/pages/PdfEditPage.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import uuid from 'react-uuid';
 import ScrollPdf from '../component/ScrollPdf';
+import { ReactComponent as AddIcon } from '../assets/add.svg';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function PdfEditPage() {
   const [file, setFile] = useState('./sample.pdf');
+  const [numPages, setNumPages] = useState(null);
+
+  const handleOnDocumentLoadSuccess = (pdf) => {
+    setNumPages(pdf.numPages);
+  };
 
   return (
     <div className="flex max-h-screen">
       <div className="flex flex-col items-center w-1/5 min-h-screen border-4 border-black overflow-y-auto">
-        <ScrollPdf docFile={file} />
+        <ScrollPdf
+          docFile={file}
+          onLoadSuccess={handleOnDocumentLoadSuccess}
+          numPages={numPages}
+        />
+
       </div>
       <div className="w-4/5" />
     </div>
