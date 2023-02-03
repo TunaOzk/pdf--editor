be8641b64359517f71c6bbb1f3e76fdd92b7c4d7
tozkoc <tunaozkoc325@gmail.com>
2023-02-03 03:28:21 +0300

    build:added .eslintignore

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [file, setFile] = useState('./sample.pdf');
+
+  const onDocumentLoadSuccess = () => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div>
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page
+        {' '}
+        {pageNumber}
+        {' '}
+        of
+        {' '}
+        {numPages}
+      </p>
+    </div>
+  );
+}
+
+export default App;
