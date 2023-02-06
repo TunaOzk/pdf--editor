3e33e105b85242798413390090ad60b500d6f23d
tozkoc <tunaozkoc325@gmail.com>
2023-02-06 14:15:22 +0300

    refactor:swapping pdf pages functionality is changed

--- src/component/PdfPage.jsx
+++ src/component/PdfPage.jsx
@@ -1,35 +1,43 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { Page } from 'react-pdf';
-import PropTypes, { number } from 'prop-types';
+import PropTypes from 'prop-types';
+import { Draggable } from '@hello-pangea/dnd';
 
 function PdfPage({
-  width, scale, pageNumber, pageIndex, dragStart, dragEnter,
+  pageNum, width, scale, index,
 }) {
+  const pageNumStr = pageNum.toString();
   return (
-    <div
-      draggable
-      onDragEnter={(e) => dragEnter(e, pageIndex)}
-      onDragStart={(e) => dragStart(e, pageIndex)}
-    >
-      <Page
-        className="rounded-md border-4 border-indigo-500"
-        renderTextLayer={false}
-        renderAnnotationLayer={false}
-        width={width}
-        scale={scale}
-        pageNumber={pageNumber}
-      />
-    </div>
+    <Draggable draggableId={pageNumStr} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...provided.draggableProps}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...provided.dragHandleProps}
+        >
+          <Page
+            className="rounded-md border-4 border-indigo-500"
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+            pageNumber={pageNum}
+            width={width}
+            scale={scale}
+          />
+        </div>
+
+      )}
+    </Draggable>
+
   );
 }
 
 PdfPage.propTypes = {
+  pageNum: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
   scale: PropTypes.number.isRequired,
-  pageNumber: PropTypes.number.isRequired,
-  pageIndex: PropTypes.number.isRequired,
-  dragStart: PropTypes.func.isRequired,
-  dragEnter: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default PdfPage;
--- src/pages/PdfEditPage.jsx
+++ src/pages/PdfEditPage.jsx
@@ -1,27 +1,63 @@
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import PropTypes, { number } from 'prop-types';
+import React, { useRef, useState } from 'react';
+import { Document, pdfjs } from 'react-pdf';
 import { useLocation } from 'react-router-dom';
-import ScrollPdf from '../component/ScrollPdf';
+import { DragDropContext, Droppable } from '@hello-pangea/dnd';
+import uuid from 'react-uuid';
+import PdfPage from '../component/PdfPage';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function PdfEditPage() {
-  const [numPages, setNumPages] = useState(null);
   const location = useLocation();
+  const fileRef = useRef({ data: location.state });
+  const [numPages, setNumPages] = useState(0);
+  const [pdfPageList, setPdfPageList] = useState([]);
 
   const handleOnDocumentLoadSuccess = (pdf) => {
     setNumPages(pdf.numPages);
+    setPdfPageList([...Array(pdf.numPages).keys()]);
+  };
+
+  const handleDragEnd = (result) => {
+    const newPageList = pdfPageList;
+    const [reorderedPage] = newPageList.splice(result.source.index, 1);
+    newPageList.splice(result.destination.index, 0, reorderedPage);
+    setPdfPageList(newPageList);
   };
 
   return (
     <div className="flex max-h-screen">
-      <div className="flex flex-col items-center w-1/5 min-h-screen border-4 border-black overflow-y-auto">
-        <ScrollPdf
-          docFile={{ data: location.state }}
-        />
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className="flex flex-col items-center w-1/5 min-h-screen border-4 border-black overflow-y-auto">
 
-      </div>
+          <Document file={fileRef.current} onLoadSuccess={handleOnDocumentLoadSuccess}>
+            <Droppable droppableId={uuid().toString()}>
+              {(provided) => (
+                <div
+                  ref={provided.innerRef}
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...provided.droppableProps}
+                  className="flex flex-col items-center"
+                >
+                  {pdfPageList.map((value, index) => (
+                    <PdfPage
+                      key={value}
+                      pageNum={value + 1}
+                      width={100}
+                      scale={2}
+                      index={index}
+                    />
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+
+            </Droppable>
+
+          </Document>
+        </div>
+      </DragDropContext>
+
       <div className="w-4/5" />
     </div>
 
