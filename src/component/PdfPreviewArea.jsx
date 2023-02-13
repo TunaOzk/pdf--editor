62f885d14c909b0a52c93909a3b005f567dd2ee3
tozkoc <tunaozkoc325@gmail.com>
2023-02-13 13:00:28 +0300

    refactor: states are moved to their components

--- src/component/PdfPreviewArea.jsx
+++ src/component/PdfPreviewArea.jsx
@@ -1,36 +1,75 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Document, Page } from 'react-pdf';
 import PropTypes, { number } from 'prop-types';
 import { ReactComponent as LoadingIcon } from '../assets/loading.svg';
+import { ReactComponent as ForwardIcon } from '../assets/arrow_forward.svg';
+import { ReactComponent as BackIcon } from '../assets/arrow_back.svg';
 
 function PdfPreviewArea({
-  file, pageList, pageNum,
+  file, noPageLeft, pageNum, setPageNum,
 }) {
+  const pdfLength = useRef(0);
+
+  const handleNavigationClickForward = () => {
+    setPageNum((prev) => (prev === pdfLength.current - 1
+      ? pdfLength.current : (prev + 1) % pdfLength.current));
+  };
+
+  const handleNavigationClickBack = () => {
+    setPageNum((prev) => (prev === 1
+      ? pdfLength.current : (prev - 1) % pdfLength.current));
+  };
+
+  const handleLoadSucces = (pdf) => {
+    pdfLength.current = pdf.numPages;
+  };
+
   const handleLoading = () => <LoadingIcon className="animate-spin" />;
   return (
-    <Document
-      file={file}
-      loading={handleLoading}
-    >
-      {pageList.length > 0 && (
-      <Page
-        className="rounded-md border-4 border-purple-500 shadow-2xl"
-        renderTextLayer={false}
-        renderAnnotationLayer={false}
-        loading={() => {}}
-        pageNumber={pageNum}
-        width={500}
-      />
-      )}
+    <div className="flex items-center">
+      <Document
+        file={file}
+        loading={handleLoading}
+        onLoadSuccess={handleLoadSucces}
+      >
+        {!noPageLeft && (
+        <Page
+          className="rounded-md border-4 border-purple-500 shadow-2xl"
+          renderTextLayer={false}
+          renderAnnotationLayer={false}
+          loading={() => {}}
+          pageNumber={pageNum}
+          width={1}
+          scale={500}
+        />
+        )}
+      </Document>
+      <button
+        onClick={handleNavigationClickForward}
+        className="transition ease-in-out duration-300 hover:bg-purple-300
+  rounded-md border-4 border-violet-400 absolute right-0"
+        type="button"
+      >
+        <ForwardIcon />
+      </button>
+      <button
+        onClick={handleNavigationClickBack}
+        className="transition ease-in-out duration-300 hover:bg-purple-300 rounded-md
+  border-4 border-violet-400 absolute left-0"
+        type="button"
+      >
+        <BackIcon />
+      </button>
+    </div>
 
-    </Document>
   );
 }
 
 PdfPreviewArea.propTypes = {
-  file: PropTypes.shape({ data: PropTypes.objectOf(number) }).isRequired,
-  pageList: PropTypes.arrayOf(number).isRequired,
+  file: PropTypes.string.isRequired,
   pageNum: PropTypes.number.isRequired,
+  noPageLeft: PropTypes.bool.isRequired,
+  setPageNum: PropTypes.func.isRequired,
 };
 
 export default PdfPreviewArea;
--- src/pages/PdfEditPage.jsx
+++ src/pages/PdfEditPage.jsx
@@ -1,9 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { pdfjs } from 'react-pdf';
 import { useLocation } from 'react-router-dom';
 import uuid from 'react-uuid';
-import { ReactComponent as ForwardIcon } from '../assets/arrow_forward.svg';
-import { ReactComponent as BackIcon } from '../assets/arrow_back.svg';
 import PdfScrollArea from '../component/PdfScrollArea';
 import PdfPreviewArea from '../component/PdfPreviewArea';
 
@@ -11,102 +9,120 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function PdfEditPage() {
   const location = useLocation();
-
   const numOfFiles = location.state.length;
-  const fileList = [...Array(numOfFiles)]
-    .map((value, index) => ({ data: location.state[index] }));
+  const fileList = location.state;
 
-  const currentPageIndex = useRef(0);
-  const currentFileIndex = useRef(0);
-
-  const [currentPdfPages, setCurrentPdfPages] = useState([]);
-  const [currentFile, setCurrentFile] = useState({ data: location.state[0] });
+  // const [currentPdfPages, setCurrentPdfPages] = useState([]);
+  const [currentFile, setCurrentFile] = useState(fileList[0]);
   const [pageNum, setPageNum] = useState(1);
-  const [pdfPagesList, setPdfPagesList] = useState([...Array(numOfFiles)].map(() => []));
-  const [selectedPdfOnDropDown, setSelectedPdfOnDropDown] = useState(0);
+  // const [pdfPagesList, setPdfPagesList] = useState([...Array(numOfFiles)].map(() => []));
+  const [fileListIndex, setFileListIndex] = useState(0);
+  const [noPagesLeftBoolean, setNoPagesLeftBoolean] = useState(false);
 
-  const handleOnDocumentLoadSuccess = (pdf) => {
-    const pdfPages = [...Array(pdf.numPages).keys()];
-    setCurrentPdfPages(() => {
-      if (!pdfPagesList[currentFileIndex.current].length) {
-        pdfPagesList[currentFileIndex.current] = pdfPages;
-        return pdfPages;
-      }
-      return pdfPagesList[currentFileIndex.current];
-    });
-    currentPageIndex.current = 0;
-    setPageNum(1);
-  };
+  // const handleOnDocumentLoadSuccess = (pdf) => {
+  //   const pdfPages = [...Array(pdf.numPages).keys()];
+  //   setCurrentPdfPages(() => {
+  //     if (!pdfPagesList[currentFileIndex.current].length) {
+  //       pdfPagesList[currentFileIndex.current] = pdfPages;
+  //       return pdfPages;
+  //     }
+  //     return pdfPagesList[currentFileIndex.current];
+  //   });
+  //   currentPageIndex.current = 0;
+  //   setPageNum(1);
+  // };
 
-  const handleDragEnd = (result) => {
-    if (!result.destination) return;
-    switch (currentPageIndex.current) {
-      case result.source.index:
-        currentPageIndex.current = result.destination.index;
-        break;
-      case result.destination.index:
-        currentPageIndex.current = result.source.index;
-        break;
-      default:
-        break;
-    }
-    const newPageList = currentPdfPages;
-    const [reorderedPage] = newPageList.splice(result.source.index, 1);
-    newPageList.splice(result.destination.index, 0, reorderedPage);
-    setCurrentPdfPages(newPageList);
-  };
+  // const handleDragEnd = (result) => {
+  //   if (!result.destination) return;
+  //   switch (currentPageIndex.current) {
+  //     case result.source.index:
+  //       currentPageIndex.current = result.destination.index;
+  //       break;
+  //     case result.destination.index:
+  //       currentPageIndex.current = result.source.index;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   const newPageList = currentPdfPages;
+  //   const [reorderedPage] = newPageList.splice(result.source.index, 1);
+  //   newPageList.splice(result.destination.index, 0, reorderedPage);
+  //   setCurrentPdfPages(newPageList);
+  // };
 
-  const handleDelete = (num, pageIndex) => {
-    setCurrentPdfPages((prevList) => prevList.filter((item, index) => item !== num - 1));
-    if (pageIndex === currentPageIndex.current) {
-      const nextPageIndex = (currentPageIndex.current + 1) % currentPdfPages.length;
-      setPageNum(currentPdfPages[nextPageIndex] + 1);
-    }
-    if (pageIndex < currentPageIndex.current) { currentPageIndex.current -= 1; }
-  };
+  // const handleDelete = (num, pageIndex) => {
+  //   setCurrentPdfPages((prevList) => {
+  //     const newList = prevList.filter((item, index) => item !== num - 1);
+  //     if (!newList.length) { setNoPagesLeftBoolean(true); }
+  //     return newList;
+  //   });
+  //   if (pageIndex === currentPageIndex.current) {
+  //     const nextPageIndex = (currentPageIndex.current + 1) % currentPdfPages.length;
+  //     setPageNum(currentPdfPages[nextPageIndex] + 1);
+  //   }
+  //   if (pageIndex < currentPageIndex.current) { currentPageIndex.current -= 1; }
+  // };
 
-  const handleClick = (e, num, index) => {
-    if (pageNum !== num) {
-      setPageNum(num);
-      currentPageIndex.current = index;
-    }
-  };
+  // const handleClick = (e, num, index) => {
+  //   if (pageNum !== num) {
+  //     setPageNum(num);
+  //     currentPageIndex.current = index;
+  //   }
+  // };
 
-  const handleNavigationClickForward = () => {
-    if (currentPdfPages.length === 1) return;
-    currentPageIndex.current = (currentPageIndex.current + 1) % currentPdfPages.length;
-    setPageNum(currentPdfPages[currentPageIndex.current] + 1);
-  };
+  // const handleNavigationClickForward = () => {
+  //   if (currentPdfPages.length === 1) return;
+  //   currentPageIndex.current = (currentPageIndex.current + 1) % currentPdfPages.length;
+  //   setPageNum(currentPdfPages[currentPageIndex.current] + 1);
+  // };
 
-  const handleNavigationClickBack = () => {
-    if (currentPdfPages.length === 1) return;
-    currentPageIndex.current = currentPageIndex.current === 0
-      ? currentPdfPages.length - 1 : (currentPageIndex.current - 1) % currentPdfPages.length;
-    setPageNum(currentPdfPages[currentPageIndex.current] + 1);
-  };
+  // const handleNavigationClickBack = () => {
+  //   if (currentPdfPages.length === 1) return;
+  //   currentPageIndex.current = currentPageIndex.current === 0
+  //     ? currentPdfPages.length - 1 : (currentPageIndex.current - 1) % currentPdfPages.length;
+  //   setPageNum(currentPdfPages[currentPageIndex.current] + 1);
+  // };
 
   const handleOptionClick = (e) => {
-    const index = e.target.value;
-    setSelectedPdfOnDropDown(index);
-    currentFileIndex.current = index;
+    const index = Number(e.target.value);
+    setFileListIndex(index);
     setCurrentFile(fileList[index]);
+    setNoPagesLeftBoolean(false);
   };
 
+  // const memoizedPdfPreviewArea = useMemo(() => (
+  //   <PdfPreviewArea
+  //     file={currentFile}
+  //     noPageLeft={noPagesLeftBoolean}
+  //     pageNum={pageNum}
+  //   />
+  // ), [currentFile, noPagesLeftBoolean, pageNum]);
+
+  const memoizedPdfScrollArea = useMemo(() => (
+    <PdfScrollArea
+      file={currentFile}
+      currFileIndex={fileListIndex}
+      setPageNum={setPageNum}
+      setNoPagesLeftBoolean={setNoPagesLeftBoolean}
+      noPageLeft={noPagesLeftBoolean}
+      numOfFiles={numOfFiles}
+    />
+  ), [currentFile, fileListIndex, noPagesLeftBoolean, numOfFiles]);
   return (
     <div className="flex h-screen bg-stone-200">
-      <PdfScrollArea
-        dragEnd={handleDragEnd}
+      {memoizedPdfScrollArea}
+      {/* <PdfScrollArea
         file={currentFile}
-        pageList={currentPdfPages}
-        docLoadSucces={handleOnDocumentLoadSuccess}
-        onDelete={handleDelete}
-        onClick={handleClick}
-      />
+        currFileIndex={currentFileIndex.current}
+        setPageNum={setPageNum}
+        setNoPagesLeftBoolean={setNoPagesLeftBoolean}
+        numOfFiles={numOfFiles}
+      /> */}
 
       <form className="relative flex flex-col items-center justify-center w-4/5">
 
         <div className="absolute top-0 left-0 rounded-md border-4 border-violet-400">
-          <select value={selectedPdfOnDropDown} onChange={handleOptionClick} name="pdfSelect">
+          <select value={fileListIndex} onChange={handleOptionClick} name="pdfSelect">
             {[...Array(numOfFiles)].map((value, index) => (
               <option key={uuid()} value={index}>
                 PDF
@@ -117,32 +133,18 @@ function PdfEditPage() {
         </div>
 
         <button
-          onClick={handleNavigationClickForward}
-          className="transition ease-in-out duration-300 hover:bg-purple-300 rounded-md border-4 border-violet-400 absolute right-0"
-          type="button"
-        >
-          <ForwardIcon />
-        </button>
-        <button
-          onClick={handleNavigationClickBack}
-          className="transition ease-in-out duration-300 hover:bg-purple-300 rounded-md border-4 border-violet-400 absolute left-0"
-          type="button"
-        >
-          <BackIcon />
-        </button>
-
-        <button
-          className="transition ease-in-out delay-150 hover:-translate-y-1
-        hover:scale-110 bg-purple-500 opacity-50 text-white hover:opacity-100 rounded-md absolute bottom-10 right-10 p-4"
+          className="transition ease-in-out delay-75 hover:-translate-y-1
+      hover:scale-110 bg-purple-500 opacity-50 text-white hover:opacity-100
+  rounded-md absolute bottom-10 right-10 p-4"
           type="button"
         >
           Merge Your All PDF Files
 
         </button>
         <button
-          className="transition ease-in-out delay-150 hover:-translate-y-1
-        hover:scale-110 bg-purple-500 opacity-50 text-white hover:opacity-100 rounded-md
-        absolute bottom-28 right-10 p-4"
+          className="transition ease-in-out delay-75 hover:-translate-y-1
+      hover:scale-110 bg-purple-500 opacity-50 text-white hover:opacity-100 rounded-md
+      absolute bottom-28 right-10 p-4"
           type="button"
         >
           Export The Current File
@@ -150,7 +152,8 @@ function PdfEditPage() {
         </button>
         <PdfPreviewArea
           file={currentFile}
-          pageList={currentPdfPages}
+          setPageNum={setPageNum}
+          noPageLeft={noPagesLeftBoolean}
           pageNum={pageNum}
         />
       </form>
